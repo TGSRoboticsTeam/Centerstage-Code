985e8ebd9fd613c0f5b9e0650325fca86c12b5af
Gobln <caasier42@gmail.com>
2024-01-17 19:22:18 -0700

    Reorganize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingClasses/ConfigClasses/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingClasses/ConfigClasses/ServoTester.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TestingClasses;
+package org.firstinspires.ftc.teamcode.TestingClasses.ConfigClasses;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingClasses/ServoPosTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingClasses/ServoPosTesting.java
@@ -2,18 +2,13 @@ package org.firstinspires.ftc.teamcode.TestingClasses;
 
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.ServoImpl;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.TestingClasses.ConfigClasses.ServoTester;
+import org.firstinspires.ftc.teamcode.TestingClasses.ConfigClasses.ServoTester2;
 
 @TeleOp(name = "Servo Testing", group = "Testing")
 @Disabled
