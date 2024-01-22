65980a3614b5f8b47dcf98caed54c4f3db9cff47
Gobln <caasier42@gmail.com>
2024-01-21 17:44:43 -0700

    linear slide subsystem should work now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Subsystems/LinearSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Subsystems/LinearSlides.java
@@ -1,7 +1,56 @@
 package org.firstinspires.ftc.teamcode.TeleOp.Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class LinearSlides extends SubsystemBase {
 
+    private final DcMotor leftSlide, rightSlide;
+    private final Servo leftArm, rightArm;
+
+    int maxHeight = -2500;
+
+    double armRotationPos = -750;
+
+    double leftArmUpPos = .61;
+    double leftArmDownPos = .83;
+
+    double rightArmUpPos = .92;
+    double rightArmDownPos = .7;
+
+    public LinearSlides(HardwareMap hardwareMap){
+        leftSlide = hardwareMap.get(DcMotor.class, "left_linear_slide");
+        rightSlide = hardwareMap.get(DcMotor.class, "right_linear_slide");
+
+        leftArm = hardwareMap.get(Servo.class, "left_claw_rotation");
+        rightArm = hardwareMap.get(Servo.class, "right_claw_rotation");
+    }
+
+    public void setPower(double upPower, double downPower){
+        if (leftSlide.getCurrentPosition() > 0) {
+            leftSlide.setPower(upPower);
+        }else if (leftSlide.getCurrentPosition() < maxHeight) {
+            leftSlide.setPower(-downPower);
+        }else{
+            leftSlide.setPower(upPower - downPower);
+        }
+
+        if (-rightSlide.getCurrentPosition() > 0) {
+            rightSlide.setPower(upPower);
+        }else if (-rightSlide.getCurrentPosition() < maxHeight) {
+            rightSlide.setPower(-downPower);
+        }else{
+            rightSlide.setPower(upPower - downPower);
+        }
+
+        if (rightSlide.getCurrentPosition() > -armRotationPos){
+            leftArm.setPosition(leftArmUpPos);
+            rightArm.setPosition(rightArmUpPos);
+        }else{
+            leftArm.setPosition(leftArmDownPos);
+            rightArm.setPosition(rightArmDownPos);
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleopSubsystemTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleopSubsystemTesting.java
@@ -2,10 +2,21 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.TeleOp.Subsystems.LinearSlides;
+
 public class TeleopSubsystemTesting extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        
+
+        LinearSlides linearSlides = new LinearSlides(hardwareMap);
+
+        while(!opModeIsActive()){
+
+        }
+
+        while(opModeIsActive()){
+            linearSlides.setPower(gamepad2.right_trigger, gamepad2.left_trigger);
+        }
     }
 }
